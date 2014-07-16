d7e6a66631a5764dfd451c18166c385c618eec4e
Elise Huard <elise@jabberwocky.eu>
2014-07-16 14:30:22 -0400

    textures added

--- src/Resurrection/Graphics.hs
+++ src/Resurrection/Graphics.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE PackageImports #-}
 module Resurrection.Graphics
-(withWindow, initGL)
+(withWindow, initGL, resizeGL, loadTexture)
 where
 
 import Graphics.Rendering.OpenGL
 import "GLFW-b" Graphics.UI.GLFW as GLFW
 import FRP.Elerea.Param
 import Control.Monad (when)
+import Data.Bitmap.Pure
+import Foreign.Ptr (castPtr)
+import Codec.Image.STB (loadImage)
 
 initGL width height = do
   clearColor $= Color4 0 0 0 1
@@ -37,3 +40,34 @@ withWindow width height title f = do
   where
     simpleErrorCallback e s =
         putStrLn $ unwords [show e, show s]
+
+compileTexture2D :: Bool -> Bool -> Bitmap Word8 -> IO (Maybe TextureObject)
+compileTexture2D isMip isClamped bitmap = do
+    mbTexName <- fmap Just genObjectName
+    textureBinding Texture2D $= mbTexName
+    let (width,height) = bitmapSize bitmap
+        wrapMode = case isClamped of
+            True    -> ClampToEdge
+            False   -> Repeat
+        (minFilter,maxLevel) = case isMip of
+            False   -> ((Linear', Nothing),0)
+            True    -> ((Linear', Just Linear'), floor $ log (fromIntegral $ max width height) / log 2)
+    textureWrapMode Texture2D S $= (Repeated, wrapMode) -- or ClampToEdge if bigger than shape. if clam s and t from 0 to 1, if repeat then depends on repeats
+    textureWrapMode Texture2D T $= (Repeated, wrapMode)
+    textureFilter Texture2D $= (minFilter, Linear')
+    textureLevelRange Texture2D $= (0, fromIntegral maxLevel)
+    withBitmap bitmap $ \(w,h) nchn 0 ptr -> do
+        let internalFormat  = RGBA8
+            dataFormat      = case nchn of
+                3   -> RGB
+                4   -> RGBA
+                _   -> error "unsupported texture format!"
+        texImage2D Texture2D NoProxy 0 internalFormat (TextureSize2D (fromIntegral w) (fromIntegral h)) 0 $ PixelData dataFormat UnsignedByte $ castPtr ptr
+    when isMip $ generateMipmap' Texture2D
+    return mbTexName
+
+loadTexture :: String -> IO (Maybe TextureObject)
+loadTexture path = do
+    Right img <- loadImage path
+    mbTexName <- compileTexture2D False True img
+    return mbTexName
